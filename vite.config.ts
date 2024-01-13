47de3357a6822aca208d2c45455a213a6167c95a
sunchaowang <sunchaowang@outlook.com>
2024-01-13 11:34:16 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { loadCurrentEnv, userConfig as baseUserConfig } from '../../vite.config.base';
+import { wrapperEnv } from '@shared/utils';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: 'module-coal',
-});
+export default ({ command, mode }) => {
+  const root = process.cwd();
+  const currentEnv = loadCurrentEnv(command, root);
+  const env = wrapperEnv(currentEnv);
+
+  console.log('root', root, env);
+
+  return defineConfig({
+    plugins: [vue()],
+    base: env.VITE_APP_NAME,
+    server: Object.assign({}, baseUserConfig.server, {
+      port: env.VITE_APP_PORT,
+    }),
+  });
+};
